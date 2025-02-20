d96bfa0329d576e13665f345b14871da81e48628
Cuile <i@cuile.com>
2025-02-20 22:54:30 +0800

    删除frp文件夹

--- Frp/frpc/bash/down.sh
+++
@@ -1,5 +0,0 @@
-docker-compose -f ../start.yml down
-
-source ports.conf
-# iptables -D INPUT -p tcp -m multiport --dport $_http_port -j ACCEPT
-# echo "关闭端口 $_bind_port,$_dashboard_port,$_http_port,$_https_port,$_allow_ports"
--- Frp/frps/bash/down.sh
+++
@@ -1,7 +0,0 @@
-docker-compose -f ../start.yml down
-
-source config
-
-iptables -D INPUT -p tcp -m multiport --dport $_bind_port,$_dashboard_port,$_http_port,$_https_port,$_allow_ports -j ACCEPT
-
-echo "关闭端口 $_bind_port,$_dashboard_port,$_http_port,$_https_port,$_allow_ports"
